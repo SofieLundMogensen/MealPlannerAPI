df951e4a20c1bd311583e13fe2aa32f8ce04f5b0
sofie <sofie@lundmogensen.dk>
2020-01-17 20:39:19 +0100

    test

--- APIFoodPlanner/Controllers/HomeController.cs
+++ APIFoodPlanner/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace APIFoodPlanner.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index() 
         {
             ViewBag.Title = "Home Page";
 
